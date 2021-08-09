fc5fc720f0f7b8c8b9b8694da9bbd9832eca62e8
Bob <bob_w@qq.com>
2021-08-09 12:57:28 +0800

    重命名

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
-const { describle, assert } = require('../src/Best');
-describle('测试Bset', () => {
+const { describle, assert } = require('../src/BoTest');
+describle('测试BoTest', () => {
   assert('测试passed', 1 === 1);
   assert('测试error', 1 === 2);
 })
